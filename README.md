4dfa76b7637b4a6a2a0e2fca2a8966647865c3d9
Adrien <verblodung@gmail.com>
2018-11-11 23:13:41 -0500

    Add glitch to ifttt request

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Create Habitica To-Dos via your favorite IFTTT trigger 
-Based on [multi-ifttt-triggers.glitch.me](https://multi-ifttt-triggers.glitch.me).
+Based on [habitica-ifttt-action.glitch.me](https://habitica-ifttt-action.glitch.me) which is in turn based on [multi-ifttt-triggers.glitch.me](https://multi-ifttt-triggers.glitch.me).
 
 # Getting Started
-You need to create a Maker service applet that notifies your Glitch app, which in turn creates a To-Do list item in Habitica. For step-by-step instructions, see `SETUP.md`. ⬅
+You need to create a Maker service applet that receives notifications from your Glitch app, which will have forwarded your Habitica events. For step-by-step instructions, see `SETUP.md`. ⬅
--- server.js
+++ server.js
@@ -1,58 +1,48 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Get the Id from IFTTT Maker URL
+if(!process.env.IFTTT_MAKER_URL)
+  console.log("You need to set your IFTTT Maker URL - copy the URL from https://ifttt.com/services/maker/settings into the .env file against 'IFTTT_MAKER_URL'");
+else
+  iftttId = process.env.IFTTT_MAKER_URL.split('https://maker.ifttt.com/use/')[1];
+
 // Show the homepage
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('views'));
 
 // Handle requests from IFTTT
 app.post("/", function (request, response) {
   console.log("Request received from IFTTT");
   console.log("Data: " + JSON.stringify(request.body));
-  
+
+  const form = {};
+  form.value1 = request.body.task.text;
+  form.value2 = request.body.task.type;
+  form.value3 = request.body.task.text;
+
+
   if (process.env.GLITCH_APP_KEY === request.body.key) {
-    console.log("Calling Habitica API...");
-    addHabiticaToDo(request.body.title);
+    console.log("Calling IFTTT.");
+    // addHabiticaToDo(request.body.title);
+    // Make a request to baseURL + triggerEvent + withKey + iftttId, which is the complete IFTTT Maker Request URL
+    request.post({url: baseURL + triggerEvent + withKey + iftttId, form: form}, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body); // Show the response from IFTTT
+      }
+    });
     console.log("Done triggering.");
   }
   else {
-    console.log("Bad key, exiting.");   
+    console.log("Bad key, exiting.");
   }
-  response.end();  
+  response.end();
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-function addHabiticaToDo(title){
-  // Make a request to Habitica using the user ID and API key from .env and the title from IFTTT
-  request({
-    headers: {
-      'x-api-user': process.env.HABITICA_USER,
-      'x-api-key': process.env.HABITICA_API_KEY
-    },
-    uri: 'https://habitica.com/api/v3/tasks/user',
-    body: { text: title, type: 'todo' },
-    json: true,
-    method: 'POST'
-  }, function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-       console.log(body); // Show the response from Habitica
-     }
-    else {
-      console.log(response.statusCode);
-      console.log(body);
-    }
-  });
-}
